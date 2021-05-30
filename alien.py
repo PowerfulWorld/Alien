b2b4a92f22183b5eccbc0a171b1c53227fce5895
KoTeDo <darkest.jerry@gmail.com>
2021-05-30 18:37:41 +0300

    Alien fleet moves to sides and down

--- alien.py
+++ alien.py
@@ -8,6 +8,7 @@ class Alien(Sprite):
         """Инициализирует пришельца и задает его изначальную позицию"""
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
 
         # Загрузка изображения пришельца и назначение атрибута rect.
         self.image = pygame.image.load('images/alien.bmp')
@@ -19,3 +20,14 @@ class Alien(Sprite):
 
         # Сохранение точной горизонтальной позции пришельца
         self.x = float(self.rect.x)
+
+    def update(self):
+        """перемещает пришельца вправо или лево"""
+        self.x += self.settings.alien_speed * self.settings.fleet_direction
+        self.rect.x = self.x
+
+    def check_edges(self):
+        """Возвращает TRUE если пришелец находится у края экрана"""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
--- alien_invasion.py
+++ alien_invasion.py
@@ -31,6 +31,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
     def _check_events(self):
@@ -104,6 +105,24 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + 2 * alien_height * row_number
         self.aliens.add(alien)
+
+    def _check_fleet_edges(self):
+        """Реагирует на достижение пришельцем края экрана"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Опускает весь флот и меняет направление движения"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+        
+    def _update_aliens(self):
+        """Обновляет позиции всех приешельцев во флоте"""
+        self._check_fleet_edges()
+        self.aliens.update()
 
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран"""
